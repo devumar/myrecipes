fe9ae0f5789e66be653c634f43b58b8df1f774d5
Muhammad Umar <landoffighter@gmail.com>
2018-10-05 18:42:15 +0500

    implement eidt and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,24 @@ class RecipesController < ApplicationController
 			render 'new'
 		end	
 	end
+	def edit
+		@recipe = Recipe.find(params[:id])	
+	end
+	def update
+		@recipe = Recipe.find(params[:id])
+		if @recipe.update(recipe_params)
+			flash[:success] = "Recipe is Updated successfully"
+			redirect_to recipe_path(@recipe)
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@recipe = Recipe.find(params[:id])
+		@recipe.destroy
+		flash[:success] ="Recipe Deleted successfully"
+		redirect_to recipes_path
+	end
 	private
 		def recipe_params
 			params.require(:recipe).permit(:name, :description)
