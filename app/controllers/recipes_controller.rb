6b38742f2c16e68101e441d284b81a29c951f753
Muhammad Umar <landoffighter@gmail.com>
2018-10-05 19:03:35 +0500

    refactor code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,11 @@
 class RecipesController < ApplicationController
-
+	before_action :set_recipe, only:[:show, :update, :destroy, :edit]
 	def index
 		@recipes = Recipe.all		
 	end
 	def show
-		@recipe = Recipe.find(params[:id])
-	end
+ 
+ 	end
 	def new
 		@recipe = Recipe.new
 	end
@@ -21,10 +21,8 @@ class RecipesController < ApplicationController
 		end	
 	end
 	def edit
-		@recipe = Recipe.find(params[:id])	
 	end
 	def update
-		@recipe = Recipe.find(params[:id])
 		if @recipe.update(recipe_params)
 			flash[:success] = "Recipe is Updated successfully"
 			redirect_to recipe_path(@recipe)
@@ -33,12 +31,14 @@ class RecipesController < ApplicationController
 		end
 	end
 	def destroy
-		@recipe = Recipe.find(params[:id])
 		@recipe.destroy
 		flash[:success] ="Recipe Deleted successfully"
 		redirect_to recipes_path
 	end
 	private
+		def set_recipe
+					@recipe = Recipe.find(params[:id])
+		end
 		def recipe_params
 			params.require(:recipe).permit(:name, :description)
 		end
